8f7ed0e5c48940fbc240bb9a2f7af3a4cc6b479e
Nishant423201 <59241834+Nishant423201@users.noreply.github.com>
2026-07-19 14:44:55 +0530

    Add PWA support for Android wrapper

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const CACHE = 'nourish-shell-v1';
+const SHELL = ['/', '/manifest.webmanifest', '/nourish-icon.svg'];
+
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
